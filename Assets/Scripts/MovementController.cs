a8b514440a6098bfcc656cabf70048db55b9499a
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2020-07-09 16:19:03 +1000

    Add unit test

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,18 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
+
+public enum Direction
+{
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+internal class KeyMapper
+{
+    private readonly Dictionary<KeyCode, Direction> _keys;
+    public List<KeyCode> movementKeys;
+
+    public KeyMapper()
+    {
+        _keys = new Dictionary<KeyCode, Direction>
+        {
+            [KeyCode.UpArrow] = Direction.Up, [KeyCode.W] = Direction.Up,
+            [KeyCode.DownArrow] = Direction.Down, [KeyCode.S] = Direction.Down,
+            [KeyCode.LeftArrow] = Direction.Left, [KeyCode.A] = Direction.Left,
+            [KeyCode.RightArrow] = Direction.Right, [KeyCode.D] = Direction.Right,
+        };
+
+        movementKeys = new List<KeyCode>(_keys.Keys);
+    }
+
+    public Direction GetDirection(KeyCode k)
+    {
+        return _keys[k];
+    }
+}
+
 
 public class MovementController : MonoBehaviour
 {
     [Tooltip("The jump force to be applied to the object when 'space' is pressed")]
-    public float jumpForce = 20.0f;
-    public float movementSpeed= 20.0f;
+    public float jumpForce = 15.0f;
 
-    
+    [Tooltip("The movement speed of the object")]
+    public float movementSpeed = 1.0f;
+
+
     // SerializeField allows var to be visible in Unity's Inspector
     [SerializeField] private bool isGrounded;
 
-
+    private KeyMapper keyMapper;
     Rigidbody rb;
 
 
@@ -20,13 +57,28 @@ public class MovementController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        keyMapper = new KeyMapper();
     }
 
+    /// <summary>
+    /// Called once per frame
+    /// </summary>
+    void Update()
+    {
+        ParseInput();
+    }
+
+    /// <summary>
+    ///  called once per frame for every collider/rigidbody that is touching rigidbody/collider.
+    /// </summary>
     void OnCollisionStay()
     {
         isGrounded = true;
     }
 
+    /// <summary>
+    /// called when this collider/rigidbody has stopped touching another rigidbody/collider.
+    /// </summary>
     void OnCollisionExit()
     {
         isGrounded = false;
@@ -35,40 +87,55 @@ public class MovementController : MonoBehaviour
     /// <summary>
     /// Translates object's transform based on wasd key press.
     /// </summary>
-    void MoveObject()
+    void ParseInput()
     {
-        float stepSize = 0.05f;
-        if (Input.GetKey(KeyCode.W))
-        {
-            Debug.Log("Move w");
-            transform.Translate(stepSize * movementSpeed , 0f, 0f);
-        }
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();
 
-        if (Input.GetKey(KeyCode.S))
+        foreach (KeyCode k in keyMapper.movementKeys)
         {
-            Debug.Log("Move s");
-            transform.Translate(-stepSize * movementSpeed, 0f, 0f);
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            Debug.Log("Move d");
-            transform.Translate(0.0f, 0f, -stepSize * movementSpeed);
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            Debug.Log("Move a");
-            transform.Translate(0.0f, 0f, stepSize * movementSpeed);
+            if (Input.GetKey(k))
+                Move(keyMapper.GetDirection(k));
         }
     }
 
-    /// <summary>
-    /// If Space pressed and object is on the floor, then allow the obj to jump
-    /// </summary>
-    void Jump()
+
+    public void Move(Direction d)
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        float stepSize = 0.05f;
+
+        if (d is Direction.Up)
+        {
+            Debug.Log("Move +x");
+            transform.Translate(stepSize * movementSpeed, 0f, 0f);
+        }
+
+        if (d is Direction.Down)
+        {
+            Debug.Log("Move -x");
+            transform.Translate(-stepSize * movementSpeed, 0f, 0f);
+        }
+
+        if (d is Direction.Left)
+        {
+            Debug.Log("Move +z");
+            transform.Translate(0.0f, 0f, +stepSize * movementSpeed);
+        }
+
+        if (d is Direction.Right)
+        {
+            Debug.Log("Move -z");
+            transform.Translate(0.0f, 0f, -stepSize * movementSpeed);
+        }
+    }
+
+
+    /// <summary>
+    /// If object is on the floor, then allow the obj to jump
+    /// </summary>
+    public void Jump()
+    {
+        if (isGrounded)
         {
             Debug.Log("<color=red>Jump</color>");
             rb.AddForce(
@@ -77,12 +144,5 @@ public class MovementController : MonoBehaviour
             );
             isGrounded = false;
         }
-    }
-
-
-    void Update()
-    {
-        MoveObject();
-        Jump();
     }
 }
---
+++ Assets/Tests/EditModeTests/TestMovementController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestMovementController
+    {
+        private MovementController _movementController;
+
+
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TestMovementControllerSimplePasses()
+        {
+            // Use the Assert class to test conditions
+            GameObject go = new GameObject();
+            go.AddComponent<Rigidbody>();
+            go.AddComponent<MovementController>();
+            Assert.IsTrue(go.GetComponent<MovementController>().jumpForce is float);
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode.
+        // In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator TestMovementControllerWithEnumeratorPasses()
+        {
+            GameObject player =
+                MonoBehaviour.Instantiate(
+                    Resources.Load<GameObject>("Prefabs/PlayerCube")
+                );
+
+            Vector3 init_postion = player.transform.position;
+            player.GetComponent<MovementController>().Move(Direction.Down);
+            player.GetComponent<MovementController>().Move(Direction.Down);
+            player.GetComponent<MovementController>().Move(Direction.Down);
+            player.GetComponent<MovementController>().Move(Direction.Down);
+            Vector3 new_postion = player.transform.position;
+            Assert.IsFalse(init_postion.Equals(new_postion));
+            yield return null;
+        }
+    }
+}
